a1628d0744a33e19cc3aec62071083e56a322122
VHKoisa <vatsalkoisa98@gmail.com>
2025-05-20 17:08:12 +0530

    created to have search as a separate entity

---
+++ next-components/app/search/page.tsx
@@ -0,0 +1,9 @@
+import SearchBox from "@/components/search/SearchBox";
+
+export default function Search() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center dark:bg-gray-900">
+        <SearchBox />
+    </div>
+  );
+}
