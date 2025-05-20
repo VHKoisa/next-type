6e8f5b244850ecdd3adce141714b82e5a05fec8e
VHKoisa <vatsalkoisa98@gmail.com>
2025-05-20 17:08:22 +0530

    minor fixes

--- next-components/components/search/SearchBox.tsx
+++ next-components/components/search/SearchBox.tsx
@@ -50,7 +50,7 @@ export default function SearchBox() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Search..."
-        className="w-full px-4 py-2 text-black rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
       
       {isLoading && (
